25384985f23e555ab19a8bbf814d51d31259d312
zhangjing9898 <1105249307@qq.com>
2017-12-11 20:42:29 +0800

    updata controller directory
    updata controller directory

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -55,3 +55,68 @@ exports.signup=function *(next) {
     }
 }
 
+exports.verify=function *(next) {
+    var verifyCode=this.request.body.verifyCode;
+    var phoneNumber=this.request.body.phoneNumber;
+
+    if(!verifyCode||!phoneNumber){
+        this.body={
+            success:false,
+            err:'验证没通过'
+        }
+        return next;
+    }
+
+    var user=yield User.findOne({
+        phoneNumber:phoneNumber,
+        verifyCode:verifyCode
+    }).exec()
+
+    if(user){
+        user.verifyCode=true;
+        user=yield  user.save();
+
+        this.body={
+            success:true,
+            data:{
+                nickname:user.nickname,
+                accessToken:user.accessToken,
+                avatar:user.avatar,
+                _id:user._id
+            }
+        }
+    }else{
+        this.body={
+            success:false,
+            err:'验证没通过'
+        }
+    }
+}
+
+exports.update=function *(next) {
+    var body=this.request.body;
+    var user=this.session.user;
+    var fields='avatar,gender,age,nickname,breed'.split(',');
+
+    fields.forEach(function (field) {
+        if(body[field]){
+            user[field]=xss(body[field].trim())
+        }
+    })
+
+    user=yield user.save();
+
+    this.body={
+        success:true,
+        data:{
+            nickname:user.nickname,
+            accessToken:user.accessToken,
+            avatar:user.avatar,
+            age:user.age,
+            breed:user.breed,
+            gender:user.gender,
+            _id:user._id
+        }
+    }
+}
+
--- config/routes.js
+++ config/routes.js
@@ -11,8 +11,13 @@ module.exports=function () {
     })
 
     //user
-    router.post('/u/signup',User.signup);
-    router.post('/u/verify',User.verify);
+    router.post('/u/signup',App.hasBody, User.signup);
+    router.post('/u/verify',App.hasBody, User.verify);
+    router.post('/u/update',App.hasBody,App.hasToken,User.update);
+
+    //app
+    router.post('signature',App.hasBody,App.hasToken,App.signature);
+
 
     return router;
 }
